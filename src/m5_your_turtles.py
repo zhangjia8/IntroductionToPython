0b677c60b5b202c23887101f8d0ddc04e40420ad
zhangj8 <zhangj8@rose-hulman.edu>
2017-11-27 10:07:34 -0500

    I changed TODO to DONE

--- src/m5_your_turtles.py
+++ src/m5_your_turtles.py
@@ -2,15 +2,15 @@
 Your chance to explore Loops and Turtles!
 
 Authors: David Mutchler, Dave Fisher, Valerie Galluzzi, Amanda Stouder,
-         their colleagues and PUT_YOUR_NAME_HERE.
+         their colleagues and Jiatong Zhang.
 """
 ########################################################################
-# TODO: 1.
+# DONE: 1.
 # On Line 5 above, replace  PUT_YOUR_NAME_HERE  with your own name.
 ########################################################################
 
 ########################################################################
-# TODO: 2.
+# DONE: 2.
 #
 #  You should have RUN the PREVIOUS module and READ its code.
 #  (Do so now if you have not already done so.)
@@ -28,3 +28,42 @@ Authors: David Mutchler, Dave Fisher, Valerie Galluzzi, Amanda Stouder,
 #
 #  Don't forget to COMMIT your work by using  VCS ~ Commit and Push.
 ########################################################################
+
+import rosegraphics as rg
+
+window = rg.TurtleWindow()
+
+red_turtle = rg.SimpleTurtle('turtle')
+red_turtle.pen = rg.Pen('red',5)
+red_turtle.speed = 15
+
+size = 30
+
+for k in range(10):
+    red_turtle.draw_circle(size)
+
+    red_turtle.pen_up()
+    red_turtle.right(45)
+    red_turtle.backward(15)
+    red_turtle.left(45)
+    red_turtle.pen_down()
+    size = size - 1
+
+green_turtle = rg.SimpleTurtle('turtle')
+green_turtle.pen = rg.Pen('green',3)
+green_turtle.speed = 10
+
+for x in range(20):
+    green_turtle.draw_regular_polygon(6,size)
+
+    green_turtle.pen_up()
+    green_turtle.right(45)
+    green_turtle.forward(20)
+    green_turtle.left(45)
+    green_turtle.pen_down()
+    size = size - 1
+
+window.close_on_mouse_click()
+
+
+
